b7441708b593e231f21a88a8d401d1bd48d6e8bc
Jan <biejan@DESKTOP-47FEMAA.localdomain>
2023-03-09 21:26:04 +0100

    Folder `AppMgmt` was renamed

--- application/main.cpp
+++ application/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "./code/utilities/AppMgmt/Application.hpp"
+#include "./code/utilities/Application/Application.hpp"
 int main()
 {
     std::cout << "Hello world!\n";
